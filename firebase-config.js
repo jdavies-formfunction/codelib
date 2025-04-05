4b36f56689c1128b51b302fa75859054a689c46a
jdavies-formfunction <verychuffed@hotmail.com>
2025-04-05 14:07:20 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,7 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    // We'll get these values from Firebase Console
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
